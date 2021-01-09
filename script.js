2acc5ec28dacb4fd18cbaa4322fa7e548f3d6207
Björk Brandt Aabling <bjorkaabling@Bjorks-MacBook-Air.local>
2021-01-09 14:47:55 +0100

    safe

--- script.js
+++ script.js
@@ -21,3 +21,49 @@ function toggleMenu() {
     }
 
 }
+//window.addEventListener("load", sidenVises);
+//
+//function sidenVises() {
+//    console.log("sidenVises");
+//
+//    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+//}
+//
+//
+//function toggleMenu() {
+//    console.log("toggleMenu")
+//
+//    document.querySelector("#menu").classList.toggle("hidden");
+//
+//    let erSkjult = document.querySelector("#menu").classList.contains("hidden");
+//
+//    if (erSkjult == true) {
+//        document.querySelector("#menuknap").textContent = "☰";
+//    } else {
+//        document.querySelector("#menuknap").textContent = "✕";
+//    }
+//
+//}
+
+function sidenVises() {
+    console.log("sidenVises");
+
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+}
+
+
+
+function toggleMenu() {
+    console.log("toggleSections")
+
+    document.querySelector("#sections").classList.toggle("hidden");
+
+    let erSkjult = document.querySelector("#sections").classList.contains("hidden");
+
+    if (erSkjult == true) {
+        document.querySelector("#menuknap").textContent = "☰";
+    } else {
+        document.querySelector("#menuknap").textContent = "✕";
+    }
+
+}
