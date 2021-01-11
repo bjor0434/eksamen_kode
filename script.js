6f8b8bd9d4032f5b689b6cb2c662ea7943ea8bb6
Björk Brandt Aabling <bjorkaabling@Bjorks-MacBook-Air.local>
2021-01-11 20:54:37 +0100

    næsten færdig

--- script.js
+++ script.js
@@ -10,60 +10,13 @@ function sidenVises() {
 function toggleMenu() {
     console.log("toggleMenu")
 
-    document.querySelector("nav").classList.toggle("hidden");
+    document.querySelector("#menu").classList.toggle("hidden");
 
-    let erSkjult = document.querySelector("nav").classList.contains("hidden");
-
-    if (erSkjult == true) {
-        document.querySelector("#menuknap").textContent = "☰";
-    } else {
-        document.querySelector("#menuknap").textContent = "✕";
-    }
-
-}
-//window.addEventListener("load", sidenVises);
-//
-//function sidenVises() {
-//    console.log("sidenVises");
-//
-//    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
-//}
-//
-//
-//function toggleMenu() {
-//    console.log("toggleMenu")
-//
-//    document.querySelector("#menu").classList.toggle("hidden");
-//
-//    let erSkjult = document.querySelector("#menu").classList.contains("hidden");
-//
-//    if (erSkjult == true) {
-//        document.querySelector("#menuknap").textContent = "☰";
-//    } else {
-//        document.querySelector("#menuknap").textContent = "✕";
-//    }
-//
-//}
-
-function sidenVises() {
-    console.log("sidenVises");
-
-    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
-}
-
-
-
-function toggleMenu() {
-    console.log("toggleSections")
-
-    document.querySelector("#sections").classList.toggle("hidden");
-
-    let erSkjult = document.querySelector("#sections").classList.contains("hidden");
+    let erSkjult = document.querySelector("#menu").classList.contains("hidden");
 
     if (erSkjult == true) {
         document.querySelector("#menuknap").textContent = "☰";
     } else {
         document.querySelector("#menuknap").textContent = "✕";
     }
-
 }
